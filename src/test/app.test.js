d64178577061d314ceb31028e5c33cd1032fd248
Lukas <lukasjurevicius1@gmail.com>
2019-01-19 22:14:36 +0200

    Adding boilerplate code

---
+++ src/test/app.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../client/app";
+
+describe("App", () => {
+  test("should match snapshot", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div").text()).toBe("Welcome to React Boilerplate App");
+    expect(wrapper).toMatchSnapshot();
+  });
+});

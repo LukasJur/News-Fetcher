bf637c7101a1f79f9860dda62ef3b117fd79e0c8
Lukas <lukasjurevicius1@gmail.com>
2019-06-24 22:37:44 +0300

    Added newsapi calls using newsapi client for node.js, backend is now able to fetch articles with the following parameters: country, q (keyword), category.

--- src/server/web.server.js
+++ src/server/web.server.js
@@ -3,8 +3,9 @@ const webpack = require('webpack');
 //const wpDevMw = require('webpack-dev-middleware');
 //const wpHotMw = require('webpack-hot-middleware');
 const webpackConfig = require('../../webpack.config');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(require('./config')['API_Key']);
 const countries = require('./countries');
-
 //const compiler = webpack(webpackConfig);
 
 export default class WebServer {
@@ -18,10 +19,29 @@ export default class WebServer {
     );*/
     //this.app.use(wpHotMw(compiler));
     //this.app.use(express.static('dist/public'));
-    this.app.get('/top-headlines/country=:country', (req, res) => {
-      const country = req.params.country;
-      console.log(country)
-      res.send(country);
+    this.app.get('/top-headlines', (req, res) => {
+      const searchQuery = {
+          country: req.query.country,
+          category: req.query.category,
+          q: req.params.q,
+      };
+      /* Remove empty, null or undefined field */
+      Object.keys(searchQuery).forEach(key => !searchQuery[key] && delete searchQuery[key]);
+      /* If searchQuery is empty or country is invalid, send 404 response */
+      if(Object.keys(searchQuery).length === 0){
+        res.status(404).send("No params")
+      }
+     console.log("search query: ", searchQuery);
+      /* Send request to newsapi */
+      newsapi.v2.topHeadlines(searchQuery)
+        .then(response => {
+          //console.log(response);
+          res.send(response)
+      })
+        .catch(error => {
+          console.log(error);
+          res.send(error);
+        });
     });
 
   }

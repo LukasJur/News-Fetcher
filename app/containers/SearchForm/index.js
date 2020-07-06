578b8976602abdc6b282adec8f6bc6e0c17d53e8
Lukas <lukasjurevicius1@gmail.com>
2020-07-06 16:05:02 +0300

    progress on search page & general structure

---
+++ app/containers/SearchForm/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
+
+import messages from './messages';
+
+const Content = () =>
+  <Paper>
+    Fetch me top news from
+  </Paper>;
+
+export default Content;
---
+++ app/containers/SearchForm/messages.js
@@ -0,0 +1,15 @@
+/*
+ * Search Page Messages
+ *
+ * This contains all the text for the SearchPage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export const scope = 'boilerplate.containers.SearchPage';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'Page not found.',
+  },
+});
